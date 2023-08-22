b999f2115768680e6ccb820db5bc7edf5d09e537
Samuel Marroni <samuelmarroni@hotmail.com>
2023-08-22 15:42:28 -0300

    [VoiceRecognizer] Finalizando o game

--- VoiceRecognizer/app/validacao.js
+++ VoiceRecognizer/app/validacao.js
@@ -15,6 +15,8 @@ function verificaSeOChuteTemUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogr Novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
@@ -30,3 +32,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero >maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload
+    }
+})
